08aba07d8c16678e48a18a23909272cd92158d88
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-11-23 17:00:27 +0100

    waluty app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const plnToEur = 4.37;
+
+  const [pln, setPln] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Waluty app</h1>
+        
+
+        <span>1 Euro to w przeliczeniu</span>
+        <h3>{plnToEur} Złoty</h3>
+
+        <label htmlFor="pln">PLN</label>
+        <input id="pln" type="number" defaultValue="3"
+          // Po dodaniu robimy controlled input -> wprost mowimy jaka ma miec wartosc
+          value={pln}
+          onChange={(e) => setPln(e.target.value)} />
+        <br />
+        {pln}
+
+        <br />
+
+        <label htmlFor="eur">EUR</label>
+        <input id="eur" type="number" value={pln / plnToEur} />
     </div>
   );
 }
