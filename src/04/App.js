6f6f125c1b2557b45cbb885a3adb36c1385ef35a
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-11-24 12:10:00 +0100

    add user

--- src/04/App.js
+++ src/04/App.js
@@ -4,6 +4,7 @@ import { UserListItem } from './users/UserListItem';
 import { findUserByEmail } from './users/findUserByEmail';
 
 function App() {
+  const [users, setUsers] = useState(apiUsers);
   const [phrase, setPhrase] = useState();
   // .filter
   //  ewentualnie nowa zmienna na przefiltrowane dane
@@ -18,6 +19,24 @@ function App() {
   //   return user.email.toLowerCase().includes(phrase.toLowerCase());
   // });
 
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const email = formData.get('email');
+
+    // tak nie robimy
+    // users.push()
+
+    // Ja nie ustawiam pozostalych pol, zeby nie zaciemniac ( first_name, itd. Ale w prawdziwej aplikacji nalezy zadbac o spojnosc danych )
+    setUsers(prev => [...prev, {
+      id: Math.random(),
+      email,
+      avatar: "https://via.placeholder.com/100x100"
+    }]);
+  }
+
   return (
     <div>
       <input
@@ -26,9 +45,15 @@ function App() {
         onChange={e => setPhrase(e.target.value)}
       />
 
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" name="email" placeholder="Email" />
+
+        <button>Dodaj</button>
+      </form>
+
       <span>Wyszukujesz: <strong>{phrase}</strong></span>
       <ul>
-        {apiUsers
+        {users
           // Bardzo czesto mozna sie spotkac z filtrowaniem tuz przed wyswietleniem ( .map() )
           // apiUsers
           // .filter(user => true)
